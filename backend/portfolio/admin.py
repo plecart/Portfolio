456f97fa73e746fba26d004f0390d4003bd178d4
plecart <pacome@lecart.dev>
2025-07-20 00:53:50 +0200

    feat: Add media handling and project model to portfolio app

--- backend/portfolio/admin.py
+++ backend/portfolio/admin.py
@@ -1,3 +1,15 @@
 from django.contrib import admin
+from .models import Project
+from django.utils.html import format_html
 
-# Register your models here.
+@admin.register(Project)
+class ProjectAdmin(admin.ModelAdmin):
+    list_display = ('title', 'professional', 'thumbnail_tag')
+    list_filter = ('professional',)
+    search_fields = ('title',)
+
+    def thumbnail_tag(self, obj):
+        if obj.thumbnail:
+            return format_html('<img src="{}" width="50" height="50" style="object-fit:cover;" />', obj.thumbnail.url)
+        return ""
+    thumbnail_tag.short_description = 'Thumbnail'
--- backend/portfolio/models.py
+++ backend/portfolio/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+
+class Project(models.Model):
+    title = models.CharField(max_length=200)
+    thumbnail = models.ImageField(upload_to='thumbnails/')
+    professional = models.BooleanField(default=False)
+
+    def __str__(self):
+        return self.title
